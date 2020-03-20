140f8fb1ffd2faea946660104f3f1d97ac505663
Andr3wRiv3rs <andrewarivers@gmail.com>
2020-03-20 14:01:49 -0700

    Enabled node integration

--- dev/index.js
+++ dev/index.js
@@ -1,9 +1,14 @@
 const { app, BrowserWindow } = require('electron')
 
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true
+
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+        nodeIntegration: true
+    },
   })
 
   mainWindow.loadURL('http://localhost:8080')
