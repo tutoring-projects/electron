031cb90d202e886bb20c4021a37d6085613a3385
Andr3wRiv3rs <andrewarivers@gmail.com>
2020-03-23 14:16:26 -0700

    Configured electron builder
    Co-authored-by: ThompsonThom <thescandanavianshop@gmail.com>

---
+++ public/index.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    // transparent: true,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+
+  mainWindow.loadFile( path.join(__dirname, 'index.html') )
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
